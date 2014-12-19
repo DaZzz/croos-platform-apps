f00111dc0d41480b4b7d76a2ac5f424899e385d9
Tikhon Belousko <dazmail01@gmail.com>
2014-12-20 01:59:01 +0300

    Написал интерфейс zipmodel.h.

--- qt-creator/lab03/zipmodel.h
+++ qt-creator/lab03/zipmodel.h
@@ -2,21 +2,25 @@
 #define ZIPMODEL_H
 
 #include <QAbstractItemModel>
+#include "zipitem.h"
 
 class ZipModel : public QAbstractItemModel
 {
     Q_OBJECT
 public:
-    explicit ZipModel(QObject *parent = 0);
+    ZipModel(const QString &fileName, Object *parent = 0);
+    ~ZipModel();
+
+    QVariant data(const QModelIndex &index, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role=Qt::DisplayRole) const;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &child) const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
 
 private:
-
-
-
-signals:
-
-public slots:
-
+    ZipItem *rootItem;
 };
 
 #endif // ZIPMODEL_H
