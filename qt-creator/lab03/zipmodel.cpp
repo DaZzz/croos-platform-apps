d39cbfabcdd873e94eccb18886a684145940a865
Tikhon Belousko <dazmail01@gmail.com>
2014-12-22 16:13:27 +0300

    Сделал модель которую можно смотреть.

--- qt-creator/lab03/zipmodel.cpp
+++ qt-creator/lab03/zipmodel.cpp
@@ -1,45 +1,80 @@
 #include "zipmodel.h"
 
-//#include <archive.h>
-//#include <archive_entry.h>
+#include <archive.h>
+#include <archive_entry.h>
 #include <QDebug>
+#include <QMap>
+#include <QDir>
 
 ZipModel::ZipModel(const QString &filePath, QObject *parent) :
     QAbstractItemModel(parent)
 {
+    rootItem = new ZipItem(tr("Корень"), 0, true);
 
-//    struct archive *a;
-//    struct archive_entry *entry;
-//    int r;
-//    const char *filePathString = filePath.toStdString().c_str();
+    struct archive *a;
+    struct archive_entry *entry;
+    int r;
+    const char *filePathString = filePath.toStdString().c_str();
 
-//    a = archive_read_new();
-//    archive_read_support_filter_all(a);
-//    archive_read_support_format_all(a);
+    a = archive_read_new();
+    archive_read_support_filter_all(a);
+    archive_read_support_format_all(a);
 
-////    r = archive_read_open_filename(a, filePathString, 10240);
-//    r = archive_read_open_filename(a, "/Users/DaZzz/Desktop/Archive.zip", 10240);
+//    r = archive_read_open_filename(a, filePathString, 10240);
+    r = archive_read_open_filename(a, "/Users/DaZzz/Desktop/folder.zip", 10240);
 
-//    if (r != ARCHIVE_OK)
-//        qDebug() << "Archive is not ok!";
+    if (r != ARCHIVE_OK)
+        qDebug() << "Archive is not ok!";
 
-//    while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-//      qDebug() << archive_entry_pathname(entry);
-//      qDebug() << archive_entry_stat(entry)->st_size;
-//      qDebug() << (QString::number(S_IFDIR, 8));
-//      qDebug() << (QString::number(archive_entry_stat(entry)->st_mode));
+    QMap<QString, ZipItem *> pathsMap;
 
-//      // Инициализация через массив
+    while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
+      bool isDir           = (archive_entry_filetype(entry) == S_IFDIR);
+      QString path         = QString::fromLocal8Bit(archive_entry_pathname(entry));
+      int size             = archive_entry_size(entry);
 
-//      archive_read_data_skip(a);
-//    }
+      if (isDir) {
+        qDebug() << "ITS FOLDER";
+        qDebug() << path;
+        path.chop(1); // удаляем последнйи слэш
+        qDebug() << path;
+      }
+
+      QStringList pathList = path.split(QDir::separator());
+
+      qDebug() << pathList;
+      QString fileName = pathList.takeLast().replace(":", QDir::separator());
+      ZipItem * item;
+      ZipItem * parent = pathsMap[pathList.join(QDir::separator())];
+
+      if (parent) {
+        item = new ZipItem(fileName, size, isDir, parent);
+        parent->appendChild(item);
+      }
+      else {
+        item = new ZipItem(fileName, size, isDir, rootItem);
+        rootItem->appendChild(item);
+      }
+      pathsMap.insert(path, item);
+
+      qDebug() << (archive_entry_filetype(entry) == S_IFDIR);
+      qDebug() << archive_entry_pathname(entry) << fileName;
+
+
+      archive_read_data_skip(a);
+    }
 
     // Инициализировать статикой
-    rootItem = new ZipItem("Корень", 0, true);
-    ZipItem *innerDir = new ZipItem("Папка", 0, true, rootItem);
-    rootItem->appendChild(innerDir);
-    rootItem->appendChild(new ZipItem("Файл", 0, false, rootItem));
-    innerDir->appendChild(new ZipItem("Вложенный файл", 0, false, innerDir));
+//    rootItem = new ZipItem("Корень", 0, true);
+//    ZipItem *innerDir = new ZipItem("Папка", 0, true, rootItem);
+//    rootItem->appendChild(innerDir);
+//    rootItem->appendChild(new ZipItem("Файл", 0, false, rootItem));
+//    innerDir->appendChild(new ZipItem("Вложенный файл", 0, false, innerDir));
+
+}
+
+void setupModel(QStringList files)
+{
 
 }
 
