b1145eeef0d356c434703723daf41059d381fb1d
Tikhon Belousko <dazmail01@gmail.com>
2014-12-29 16:12:02 +0300

    Сделал красивый билд для Cmake.

--- projects/lab01/_build/build.sh
+++ projects/lab01/_build/build.sh
@@ -1,4 +1,19 @@
+###
+# Make build
+###
 cd task13
 pwd
 make
 ../_build/test-sort
+
+
+###
+# Cmake build
+###
+
+cd _build
+mkdir -p cmake-build
+cd cmake-build
+cmake ../../task13/
+make
+./test-sort/test-sort
